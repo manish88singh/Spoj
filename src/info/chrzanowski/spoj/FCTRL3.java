91894ba4a0543b7cc3b2e4d64e20d8484502f736
hsz <jakub@chrzanowski.info>
2012-09-13 13:05:55 +0200

    FCTRL3: zaakceptowano, czas: 0.28

--- src/info/chrzanowski/spoj/FCTRL3.java
+++ src/info/chrzanowski/spoj/FCTRL3.java
@@ -10,7 +10,7 @@ import java.io.InputStreamReader;
  * Kod zadania: FCTRL3
  * Poziom:      łatwy
  * Status:      zaakceptowano
- * Czas:        0.29
+ * Czas:        0.28
  *
  * @see <a href="http://pl.spoj.pl/problems/FCTRL3/">http://pl.spoj.pl/problems/FCTRL3/</a>
  * @author Jakub Chrzanowski
@@ -24,42 +24,43 @@ public class FCTRL3 {
         while (counter-- > 0) {
             int input = Integer.parseInt(reader.readLine());
 
-            switch (input) {
+            if ( input < 10 ) {
+                switch (input) {
 
-                case 0:
-                case 1:
-                    System.out.println("0 1");
-                    break;
+                    case 0:
+                    case 1:
+                        System.out.println("0 1");
+                        break;
 
-                case 2:
-                    System.out.println("0 2");
-                    break;
+                    case 2:
+                        System.out.println("0 2");
+                        break;
 
-                case 3:
-                    System.out.println("0 6");
-                    break;
+                    case 3:
+                        System.out.println("0 6");
+                        break;
 
-                case 4:
-                    System.out.println("2 4");
-                    break;
+                    case 4:
+                        System.out.println("2 4");
+                        break;
 
-                case 5:
-                case 6:
-                case 8:
-                    System.out.println("2 0");
-                    break;
+                    case 5:
+                    case 6:
+                    case 8:
+                        System.out.println("2 0");
+                        break;
 
-                case 7:
-                    System.out.println("4 0");
-                    break;
+                    case 7:
+                        System.out.println("4 0");
+                        break;
 
-                case 9:
-                    System.out.println("8 0");
-                    break;
+                    case 9:
+                        System.out.println("8 0");
+                        break;
 
-                default:
-                    System.out.println("0 0");
-
+                }
+            } else {
+                System.out.println("0 0");
             }
 
         }
